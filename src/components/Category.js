4c12bb707a5b1593f10ecad593e41e9e440100d6
Tania Omelko <tanyaomelko39@gmail.com>
2023-12-07 14:10:08 +0200

    add router

---
+++ src/components/Category.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectCategories } from '../features/categories/categoriesSlice';
+import { selectArticles } from '../features/articles/articlesSlice';
+import { Link, useParams, Outlet } from 'react-router-dom';
+
+export default function Category () {
+  const categories = useSelector(selectCategories)
+  const articles = useSelector(selectArticles)
+
+  const { name } = useParams()
+
+  return (
+    <div>
+      <h2>Articles Related to {name}</h2>
+      <ul>
+        { categories[name].map(articleSlug => (
+          <li key={articles[articleSlug].title}>
+            <Link to={`/articles/${articleSlug}`}>{articles[articleSlug].title}</Link>
+          </li>
+        ))}
+      </ul>
+      <Outlet />
+    </div>
+  )
+}
