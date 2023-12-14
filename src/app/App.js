e93ca47cdb7f382c8a9fc5f20487cf7fb05e66e2
Tania Omelko <tanyaomelko39@gmail.com>
2023-12-14 22:26:40 +0200

    add PUBLIC_URL

--- src/app/App.js
+++ src/app/App.js
@@ -27,7 +27,9 @@ const router =
         <Route path="edit" element={ <EditProfileForm/> } />
       </Route>
     </Route>
-  )
+  ), {
+    basename: process.env.PUBLIC_URL
+  }
   );
 
 function App() {
